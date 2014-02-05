52d60df635807e094eca5de5ea3335541523d668
anderfernandes <handerson171@gmail.com>
2014-02-04 19:26:55 -0600

    Circle
    Circle HTML/JS

---
+++ circle/circle.js
@@ -0,0 +1,19 @@
+﻿function Circle(r) {
+    this.radius = r;
+    this.getArea = getA;
+    this.getPerimeter = getP;
+    this.getData = getD;
+}
+
+function getA() {
+    return this.radius * this.radius * Math.PI;
+}
+
+function getP() {
+    return 2 * this.radius * Math.PI;
+}
+
+function getD() {
+    return 'Area: ' + this.getArea() + '<br />Perimeter: ' +
+        this.getPerimeter();
+}
---
+++ circle/controller.js
@@ -0,0 +1,15 @@
+﻿function calculate() {
+    var r = parseFloat(document.getElementById('radius').value);
+    if (isNaN(r)) {
+        document.getElementById('data').innerHTML =
+            "Please enter numbers only";
+        return;
+    }
+    if (r <= 0) {
+        document.getElementById('data').innerHTML =
+            "Negative numbers don't make sense";
+        return;
+    }
+    var myCircle = new Circle(r);
+    document.getElementById('data').innerHTML = myCircle.getData();
+}
